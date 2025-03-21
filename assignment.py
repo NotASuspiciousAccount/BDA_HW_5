dfd15a46016b4d3b6da806c5f635f5de0ddd434a
maanya-naveen-kr <100097596+mangopudding03@users.noreply.github.com>
2025-03-21 04:37:37 +0000

    COMMIT #2

--- assignment.py
+++ assignment.py
@@ -3,15 +3,17 @@ import pandas as pd
 import sklearn
 
 from sklearn.decomposition import TruncatedSVD
-
-file = "ratingmatrix_BDA.xlsx"
-importmatrix = pd.read_excel(file, header=0, index_col=0)
-ratings_matrix = importmatrix.fillna(0).to_numpy()
+from sklearn.metrics import mean_squared_error
 
 # Question 1:
 # G = 11
 # R1 = 2, R2 = 9, R3 = 6
 # C1 = 2, C2 = 2, C3 = 6
+
+file = "ratingmatrix_BDA.xlsx"
+matrix = pd.read_excel(file, header=0, index_col=0)
+ratings_matrix = matrix.fillna(0).to_numpy()
+print(ratings_matrix)
 
 svd = TruncatedSVD(n_components=3)
 U = svd.fit_transform(ratings_matrix)
@@ -19,7 +21,23 @@ sigma = np.diag(svd.singular_values_)
 V = svd.components_
 
 # Q5 
-# Matrix U is essentially 
+# Matrix U is the user feature matrix that represents how each strongly each user(rows) relates to the latent factors
 print("U Matrix: \n", U)
+# Matrix Sigma is a diagnol matrix that represents the importance of each latent factor. 
+# The larger the singular value, the more influential that latent factor is in explaining the variability or structure in the ratings matrix.
 print("\n Sigma: \n", sigma)
+# The V matrix tells us how each movie relates to the latent factors
 print("\n V Matrix: \n" , V)
+
+# Q6
+# Approximatation of the ratings matrix
+approx_ratingMatrix = np.dot(U, np.dot(sigma, V))
+print("\nApproximate Ratings Matrix: \n", approx_ratingMatrix)
+
+# Q7
+# No vacant cells only entries with values
+known_ratings_mask = ratings_matrix > 0
+
+# Calculate the RMSE considering only known ratings and no vacant cells
+rmse = np.sqrt(mean_squared_error(ratings_matrix[known_ratings_mask], approx_ratingMatrix[known_ratings_mask]))
+print("\nRoot Mean Squared Error (RMSE) between original and approximate matrix: ", rmse)
